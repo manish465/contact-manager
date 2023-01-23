69402ec3583d1f4b167aaa3da33f8a325748b471
manish <46858945+manish465@users.noreply.github.com>
2023-01-23 11:32:19 +0530

    Initial commit

---
+++ src/test/java/com/manish/contactmanager/ContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.contactmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
